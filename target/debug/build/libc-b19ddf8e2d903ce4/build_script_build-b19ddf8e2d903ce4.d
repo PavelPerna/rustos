379d58ed48d877e2f98893ede7fee8605ef8a981
PavelPerna <147667690+PavelPerna@users.noreply.github.com>
2025-01-31 10:52:54 +0100

    Delete target directory

--- target/debug/build/libc-b19ddf8e2d903ce4/build_script_build-b19ddf8e2d903ce4.d
+++
@@ -1,5 +0,0 @@
-/mnt/d/Devel/rustlib/testing/target/debug/build/libc-b19ddf8e2d903ce4/build_script_build-b19ddf8e2d903ce4: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.169/build.rs
-
-/mnt/d/Devel/rustlib/testing/target/debug/build/libc-b19ddf8e2d903ce4/build_script_build-b19ddf8e2d903ce4.d: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.169/build.rs
-
-/root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.169/build.rs:
