8a6d0ea707643039ef1035d245f39e2cc019821e
PavelPerna <147667690+PavelPerna@users.noreply.github.com>
2025-08-20 11:38:19 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -28,12 +28,12 @@ pub  fn readwrite()-> !{
 
     
     loop{
-        let len :i32 = stdin.readln(buf).expect("Error reading from STDIN !!!");
+        let len :i32 = stdin.readln(buf).expect("I/O(READ) Error");
         if len > 0 {
-            let data = core::str::from_utf8(&buf[0..len as usize]).expect("Pica");
-            let _ = write!(&mut stdout,"Nactena data: Delka({}) , Obsah({})",len,data);
+            let data = core::str::from_utf8(&buf[0..len as usize]).expect("I/O(WRITEBUFFER) Error");
+            let _ = write!(&mut stdout,"I/O:READ:DEBUG='{}'({})",data,len);
         }else{  
-            let _ = write!(&mut stdout,"Chyba  pico !!!" );
+            let _ = write!(&mut stdout,"I/O(READ)[ERROR=Data Len]:'{}'(0)", data);
         }    
     }
 }
